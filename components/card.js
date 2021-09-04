cd75e7524ef5bef0ff6bdd4c6a8e9dd6525c5f7a
Gabriel Fontanilla <gfontan1@my.hpu.edu>
2021-09-04 10:26:41 -1000

    init commit

--- components/card.js
+++ components/card.js
@@ -1,5 +1,6 @@
 import styles from "./css/Card.module.css";
 import festival_pic from "../public/food_festival.jpg";
+
 export function Card() {
   return (
     <section className={styles.container}>
@@ -7,10 +8,8 @@ export function Card() {
         <img className={styles.image_container} src={festival_pic} />
       </div>
       <figure className={styles.text_container}>
-        <h3 style={{ fontWeight: 600, marginBottom: "0.5rem" }}>
-          Ala Moana Food Festival
-        </h3>
-        <summary style={{ color: "#646464" }}>August 3rd, 2021</summary>
+        <h5 className={styles.mb_half}>Ala Moana Food Festival</h5>
+        <summary>August 3rd, 2021</summary>
       </figure>
     </section>
   );
